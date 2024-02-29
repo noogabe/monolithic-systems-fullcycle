56b1f22ef6414b92d4b8031bd3ccd4b587dde914
Gabriele Almeida <naogabe@gmail.com>
2024-02-29 13:19:59 -0300

    feat: add invoice module

---
+++ src/modules/invoice/factory/facade.factory.ts
@@ -0,0 +1,19 @@
+import { InvoiceFacade } from "../facade/invoice.facade";
+import { InvoiceRepository } from "../repository/invoice.repository";
+import { FindInvoiceUseCase } from "../usecase/find/find-invoice.usecase"
+import { GenerateInvoiceUseCase } from "../usecase/generate/generate-invoice.usecase";
+
+export class InvoiceFacadeFactory {
+  static create(): InvoiceFacade {
+    const repository = new InvoiceRepository();
+    const findInvoiceUseCase = new FindInvoiceUseCase(repository);
+    const generateInvoiceUseCase = new GenerateInvoiceUseCase(repository);
+
+    const invoiceFacade = new InvoiceFacade({
+      findUseCase: findInvoiceUseCase,
+      generateUseCase: generateInvoiceUseCase,
+    });
+
+    return invoiceFacade;
+  }
+}
